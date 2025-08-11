cbab073adc4247b8005fe21a4f086738eed13370
riveirasanti <riveirasanti@gmail.com>
2025-08-11 18:46:30 -0300

    Add files via upload

--- exercise_math.py
+++ exercise_math.py
@@ -1,3 +1,17 @@
 def math():
     a = 57
     b = 7
+    print(a+b) #suma 
+    resta=a-b
+    print(resta) #resta 
+    multiplicacion=a*b
+    print(multiplicacion) #producto
+    promedio= (a+b)/2
+    print(promedio) #promedio
+    cociente = a//b 
+    print(cociente) #cociente
+    resto=a%b
+    print(resto) #resto
+    valor_real=a/b
+    print(valor_real) #valor real
+math()
